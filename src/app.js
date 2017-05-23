d91e9e246524a57932ef0409fda21c0a8e3fdddf
Ishan Manandhar <ishan.manandhar@wolfmatrix.com>
2017-05-23 22:17:14 +0545

    jquery imported into webpack

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
 import css from './app.scss';
+import $ from "jquery";
 
 console.log('test');
+
+$(document).ready(function(){
+    $("h6").click(function(){
+        $(this).hide();
+    });
+});
